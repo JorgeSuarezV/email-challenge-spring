6d28c363e8ac210c4586a4d69a570d081f4da42c
Santiago Encina <santiago.encina@ing.asutral.edu.ar>
2023-06-11 15:12:43 -0300

    security auto config removed

--- src/main/java/challenge/emailservicespring/EmailServiceSpringApplication.java
+++ src/main/java/challenge/emailservicespring/EmailServiceSpringApplication.java
@@ -2,8 +2,9 @@ package challenge.emailservicespring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class EmailServiceSpringApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/challenge/emailservicespring/domains/health/controller/HealthController.java
@@ -0,0 +1,17 @@
+package challenge.emailservicespring.domains.health.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path ="health")
+@CrossOrigin(origins = "*")
+public class HealthController {
+    @GetMapping
+    public ResponseEntity<String> getHealth(){
+        return ResponseEntity.ok("Server running");
+    }
+}
